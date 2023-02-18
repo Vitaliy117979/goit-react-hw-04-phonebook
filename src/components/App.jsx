01dfd586686a71740533987205219dfda38c98fe
Vitaliy117979 <108054894+Vitaliy117979@users.noreply.github.com>
2023-02-18 09:58:44 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,15 +5,29 @@ import { Form } from 'components/Form/Form';
 import { ContactList } from './ContactList/ContactList';
 import { MainWrapper } from './MainWrapper.styled';
 import { Filter } from './Filter/Filter';
+const INITIAL_CONTACTS = [
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
+
+const getInitialContacts = () => {
+  const parsedContacts = JSON.parse(localStorage.getItem('contacts'));
+  if (parsedContacts) {
+    return parsedContacts;
+  } else {
+    return INITIAL_CONTACTS;
+  }
+};
+
+
 export const App = () => {
-  const [contacts, setContacts] = useState(
-    () => JSON.parse(localStorage.getItem('contacts')) ?? ''
-  );
-
+  const [contacts, setContacts] = useState(getInitialContacts);
   const [filter, setFilter] = useState('');
-
   useEffect(() => {
     window.localStorage.setItem('contacts', JSON.stringify(contacts));
+    console.log(contacts);
   }, [contacts]);
 
   const onHandlerSubmit = (name, number) => {
